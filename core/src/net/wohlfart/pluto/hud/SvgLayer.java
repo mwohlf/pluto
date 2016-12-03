35cf2e21c9879489a9bb716844ca241ddb288cb3
michael <michael@woodstock>
2016-12-03 14:55:01 +0100

    final fixes to get desktop running at least

--- core/src/net/wohlfart/pluto/hud/SvgLayer.java
+++ core/src/net/wohlfart/pluto/hud/SvgLayer.java
@@ -1,35 +1,13 @@
 package net.wohlfart.pluto.hud;
 
-import com.badlogic.gdx.Gdx;
-import org.lwjgl.nanovg.NVGColor;
-
-import static org.lwjgl.nanovg.NanoVG.*;
-import static org.lwjgl.nanovg.NanoVGGL2.*;
-
 public class SvgLayer implements HudLayer {
 
-    final long ctx;
-    private final NVGColor colorA = NVGColor.create().r(1).g(0).b(0).a(0.6f);
 
     public SvgLayer() {
-        ctx = nnvgCreateGL2(NVG_ANTIALIAS | NVG_STENCIL_STROKES | NVG_DEBUG);
     }
 
     @Override
     public void render() {
-        final int width = Gdx.graphics.getWidth();
-        final int height = Gdx.graphics.getHeight();
-
-        nvgBeginFrame(ctx, width, height, 1.0f);
-        nvgSave(ctx);
-
-        nvgBeginPath(ctx);
-        nvgRoundedRect(ctx, 30, 30, 340, 30, 5);
-        nvgFillColor(ctx, colorA);
-        nvgFill(ctx);
-
-        nvgRestore(ctx);
-        nvgEndFrame(ctx);
 
     }
 
